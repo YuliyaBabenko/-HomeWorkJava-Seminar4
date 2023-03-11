c07168c543e6557059b00c288a575eb25bb61521
Yuliya <ju7iab@gmail.com>
2023-03-11 17:25:52 +0300

    with console feedback

--- phones.java
+++ phones.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 // HashMap<Login, Список телефонов>
@@ -6,7 +9,8 @@ import java.util.*;
 // - находить по логину телефон/список телефонов
 
 public class phones {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
         Map<String, HashSet<String>> phonesList = new HashMap<String, HashSet<String>>();
 
         String log1 = "Ivanov";
@@ -28,7 +32,6 @@ public class phones {
         pList3.add("+79198745830");
         pList3.add("+79651963937");
         pList3.add("+79119457701");
-        pList3.remove("+79651963937");
         pList3.add("+79651963931");
 
         HashSet<String> pList4 = new HashSet<String>();
@@ -41,6 +44,72 @@ public class phones {
         phonesList.put(log3, pList3);
         phonesList.put(log4, pList4);
 
-        System.out.printf("For " + log3 + " we have the following phone list: " + phonesList.get(log3));
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter you Login: ");
+        String log = bf.readLine();
+        System.out.println("Enter action: output, add, delete, exit");
+        String act = bf.readLine();
+
+        while(!act.equals("exit")){
+            if(act.equals("add")){
+                System.out.println("Enter a phone number:");
+                String phone = bf.readLine();
+                if (log.equals(log1)) {
+                    pList1.add(phone);
+                }
+                else if (log.equals(log2)) {
+                    pList2.add(phone);
+                }
+                else if (log.equals(log3)) {
+                    pList3.add(phone);
+                }
+                else if (log.equals(log4)) {
+                    pList4.add(phone);
+                }
+                else {
+                    System.out.println("The Login is not valid");
+                }
+            }else{
+                if(act.equals("delete")){
+                    System.out.println("Enter a phone number:");
+                    String phone = bf.readLine();
+                    if (log.equals(log1)) {
+                        pList1.remove(phone);
+                    }
+                    else if (log.equals(log2)) {
+                        pList2.remove(phone);
+                    }
+                    else if (log.equals(log3)) {
+                        pList3.remove(phone);
+                    }
+                    else if (log.equals(log4)) {
+                        pList4.remove(phone);
+                    }
+                    else {
+                        System.out.println("The Login is not valid");
+                    }
+                }else{
+                    if (act.equals("output")){
+                        if (log.equals(log1)) {
+                            System.out.println(phonesList.get(log1));
+                        }
+                        else if (log.equals(log2)) {
+                            System.out.println(phonesList.get(log2));
+                        }
+                        else if (log.equals(log3)) {
+                            System.out.println(phonesList.get(log3));
+                        }
+                        else if (log.equals(log4)) {
+                            System.out.println(phonesList.get(log4));
+                        }
+                        else {
+                            System.out.println("The Login is not valid");
+                        }
+                    }
+                }
+            }
+            System.out.println("Enter action: output, add, delete, exit");
+            act = bf.readLine();
+        }
     }
 }
